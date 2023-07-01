157560d995307d3e9f88691aded618a7eb380eb3
Armaan Gupta <armaan@guppy.im>
2023-07-01 16:42:48 -0500

    ref: moved to pages directory

---
+++ pages/index.tsx
@@ -0,0 +1,16 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Kreative Horizon</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        <h1>Welcome to my Next.js App</h1>
+      </main>
+    </div>
+  );
+}
