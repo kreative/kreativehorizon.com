6779d0aeb54385a4eb313be4496dfc5608581575
Armaan Gupta <armaan@guppy.im>
2023-07-06 15:18:19 -0500

    feat: defaults + tester homepage

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,87 @@
+const plugin = require("tailwindcss/plugin");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: ["class"],
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,jsx,ts,tsx}",
+    "./components/**/*.{js,jsx,ts,tsx}",
+    "./app/**/*.{js,jsx,ts,tsx}",
+    "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        "horizon-orange": "#FF900E",
+        "horizon-pink": "#FF0E9F",
+        "horizon-purple": "#950EFF",
+        "horizon-grey-100": "#FAFAFA",
+        "horizon-grey-200": "#F6F6F4",
+        "horizon-grey-300": "#E3E3E3",
+        "horizon-grey-400": "#C9C9C9",
+        "horizon-grey-500": "#959093",
+        "horizon-grey-600": "#706B6E",
+        "horizon-grey-700": "#4B484A",
+        "horizon-grey-800": "#1F1D1E",
+      },
+      gradientColorStops: {
+        "horizon-orange": "#FF900E",
+        "horizon-pink": "#FF0E9F",
+        "horizon-purple": "#950EFF",
+      },
+      keyframes: {
+        enterFromRight: {
+          from: { opacity: 0, transform: "translateX(200px)" },
+          to: { opacity: 1, transform: "translateX(0)" },
+        },
+        enterFromLeft: {
+          from: { opacity: 0, transform: "translateX(-200px)" },
+          to: { opacity: 1, transform: "translateX(0)" },
+        },
+        exitToRight: {
+          from: { opacity: 1, transform: "translateX(0)" },
+          to: { opacity: 0, transform: "translateX(200px)" },
+        },
+        exitToLeft: {
+          from: { opacity: 1, transform: "translateX(0)" },
+          to: { opacity: 0, transform: "translateX(-200px)" },
+        },
+        scaleIn: {
+          from: { opacity: 0, transform: "rotateX(-10deg) scale(0.9)" },
+          to: { opacity: 1, transform: "rotateX(0deg) scale(1)" },
+        },
+        scaleOut: {
+          from: { opacity: 1, transform: "rotateX(0deg) scale(1)" },
+          to: { opacity: 0, transform: "rotateX(-10deg) scale(0.95)" },
+        },
+        fadeIn: {
+          from: { opacity: 0 },
+          to: { opacity: 1 },
+        },
+        fadeOut: {
+          from: { opacity: 1 },
+          to: { opacity: 0 },
+        },
       },
     },
+    animation: {
+      scaleIn: "scaleIn 200ms ease",
+      scaleOut: "scaleOut 200ms ease",
+      fadeIn: "fadeIn 200ms ease",
+      fadeOut: "fadeOut 200ms ease",
+      enterFromLeft: "enterFromLeft 250ms ease",
+      enterFromRight: "enterFromRight 250ms ease",
+      exitToLeft: "exitToLeft 250ms ease",
+      exitToRight: "exitToRight 250ms ease",
+    },
   },
-  plugins: [],
-}
+  plugins: [
+    plugin(({ matchUtilities }) => {
+      matchUtilities({
+        perspective: (value) => ({
+          perspective: value,
+        }),
+      });
+    }),
+  ],
+};
