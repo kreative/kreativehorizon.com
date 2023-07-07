debad8522a71b9b62713f0cd7599f1900014815f
Armaan Gupta <armaan@guppy.im>
2023-07-07 10:14:11 -0500

    chore: etc

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,7 +5,7 @@ export default function Home() {
     <div>
       <Navbar />
       <div className={"min-h-screen bg-red-500 pt-24"}>
-        <h1 className="text-xl text-center">My Site</h1>
+        <h1 className="font-chedros text-xl text-center">My Site</h1>
       </div>
     </div>
   );
