33b453816069f683bd3c5942814f73eb96021e21
Armaan Gupta <armaan@guppy.im>
2023-07-07 12:11:12 -0500

    feat: created hellobar

---
+++ components/HelloBar.tsx
@@ -0,0 +1,23 @@
+import Link from "next/link"
+import { useState } from "react";
+import Marquee from "react-fast-marquee";
+
+export default function HelloBar({
+  headline,
+  link,
+}: {
+  headline: string;
+  link: string;
+}) {
+  return (
+    <div className="bg-horizon-grey-800 py-1">
+      <Marquee speed={25} autoFill={true} pauseOnHover={true} direction="right">
+        <div className="px-6 md:px-10">
+          <Link href={link} className="text-white text-sm font-mono hover:underline italic">
+            {headline}
+          </Link>
+        </div>
+      </Marquee>
+    </div>
+  );
+}
