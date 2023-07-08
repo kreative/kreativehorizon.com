5f542a03b3065175abe7f9f1906a1987d61c3e2c
Armaan Gupta <armaan@guppy.im>
2023-07-08 09:49:12 -0500

    ref: couldn't get the popup menu to work with HelloBar

--- components/PopupMenu.tsx
+++ components/PopupMenu.tsx
@@ -1,7 +1,8 @@
+import React, { useState } from 'react';
 import Link from "next/link";
 import { List, X } from "@phosphor-icons/react";
-import { useState } from "react";
 import { Dialog } from "@headlessui/react";
+
 
 function MenuItem({ link, name }) {
   return (
@@ -35,7 +36,7 @@ export default function PopupMenu() {
         className="relative z-50"
       >
         {/* <div className="fixed inset-0 bg-black/30" aria-hidden="true" /> */}
-        <div className="fixed inset-0 flex items-start justify-start p-4 top-[4rem]">
+        <div className={`fixed inset-0 flex items-start justify-start p-4 top-[4rem]`}>
           <Dialog.Panel className="mx-auto min-w-full rounded-lg bg-white bg-opacity-[.95] backdrop-blur-sm shadow-lg">
             <div className="flex flex-col items-start justify-between space-y-1 p-5">
               <MenuItem link={"/Events"} name={"Events"} />
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,8 @@
-import HelloBar from "../components/HelloBar";
 import Navbar from "../components/Navbar";
 
 export default function Home() {
   return (
     <div>
-      <HelloBar
-        headline="HackISU v2 is on it&apos;s way... click here to register now ->"
-        link="/events/hackisu-v2"
-      />
       <Navbar />
       <div className={"min-h-screen bg-red-500 pt-24"}>
         <h1 className="font-chedros text-xl text-center">My Site</h1>
