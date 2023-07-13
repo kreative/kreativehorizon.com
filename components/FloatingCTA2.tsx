6e462e95f000ddac5cbca828adcaade878d6b0bd
Armaan Gupta <armaan@guppy.im>
2023-07-13 00:13:35 -0500

    ref: mobile responsiveness

--- components/FloatingCTA2.tsx
+++ components/FloatingCTA2.tsx
@@ -23,33 +23,37 @@ export default function FloatingCTA2({
   return (
     <Container>
       <div className="bg-horizon-grey-200 rounded-3xl p-16">
-        <div className="grid grid-cols-12 gap-0">
-          <div className="col-span-7 flex flex-col justify-center space-y-2">
+        <div className="grid grid-cols-12 gap-0 ">
+          <div className="col-span-12 sm:col-span-7 flex flex-col justify-center space-y-2">
             <p className="text-2xl tracking-wide font-guthen">{label}</p>
             <h1 className="text-5xl md:text-6xl font-chedros text-left">
               {title}
             </h1>
-            <p className="text-base text-horizon-grey-600 pb-6">{description}</p>
-            <motion.a
-              whileHover={{ scale: 0.97, transition: { duration: 0.2 } }}
-              whileTap={{ scale: 0.95 }}
-            >
-              <Link
-                href={btnLink}
-                className="rounded-md py-2.5 px-5 font-bold bg-horizon-pink text-white"
+            <p className="text-base text-horizon-grey-600 pb-6">
+              {description}
+            </p>
+            <div className="flex justify-start items-center">
+              <motion.div
+                whileHover={{ scale: 0.97, transition: { duration: 0.2 } }}
+                whileTap={{ scale: 0.95 }}
               >
-                {btnLabel}
-              </Link>
-            </motion.a>
+                <Link
+                  href={btnLink}
+                  className="rounded-md py-2.5 px-5 font-bold bg-horizon-pink text-white shadow-md"
+                >
+                  {btnLabel}
+                </Link>
+              </motion.div>
+            </div>
           </div>
-          <div className="col-span-1" />
-          <div className="col-span-4">
+          <div className="col-span-12 sm:col-span-1" />
+          <div className="col-span-12 sm:col-span-4 flex justify-end items-center">
             <Image
               src={imageSrc}
               alt={imageAlt}
               width={500}
               height={500}
-              className="w-full h-auto"
+              className="-mt-10 sm:m-0 w-[60%] sm:w-full h-auto"
             />
           </div>
         </div>
--- components/LogoCloud5.tsx
+++ components/LogoCloud5.tsx
@@ -10,41 +10,41 @@ export default function LogoCloud5() {
             Trusted by the world&apos;s most mission driven companies
           </p>
         </div>
-        <div className="col-span-12 xl:col-span-8 flex flex-wrap space-x-6 lg:justify-center xl:justify-end items-center">
+        <div className="col-span-12 xl:col-span-8 flex flex-wrap sm:space-x-6 space-y-3 sm:space-y-0 justify-center xl:justify-end items-baseline sm:items-center">
           <Image
             src="/images/sponsors/PUSHER.png"
             alt="Pusher Logo in Color"
             width={7000}
             height={100}
-            className="w-1/6 h-auto"
+            className="mr-4 sm:mr-0 w-1/4 sm:w-1/6 h-auto"
           />
           <Image
             src="/images/sponsors/MORNINGSTAR.png"
             alt="Morningstar Logo in Color"
             width={7000}
             height={100}
-            className="w-1/6 h-auto"
+            className="mr-4 sm:mr-0 w-1/4 sm:w-1/6 h-auto"
           />
           <Image
             src="/images/sponsors/BALSAMIQ.png"
             alt="Balsamiq Logo in Color"
             width={7000}
             height={100}
-            className="w-1/6 h-auto"
+            className="w-1/4 sm:w-1/6 h-auto"
           />
           <Image
             src="/images/sponsors/MICROSOFT.png"
             alt="Microsoft Logo in Color"
             width={7000}
             height={100}
-            className="w-1/6 h-auto"
+            className="mr-4 sm:mr-0 w-1/4 sm:w-1/6 h-auto"
           />
           <Image
             src="/images/sponsors/IVY_COLLEGE.png"
             alt="Ivy College of Business Start Something Logo in Color"
             width={7000}
             height={100}
-            className="w-1/6 h-auto"
+            className="w-1/4 sm:w-1/6 h-auto"
           />
         </div>
       </div>
