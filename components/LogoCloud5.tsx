d10be461910e8134b307ae3ec7ab283210058449
Armaan Gupta <armaan@guppy.im>
2023-07-12 22:33:37 -0500

    feat: created logo cloud 5

---
+++ components/LogoCloud5.tsx
@@ -0,0 +1,53 @@
+import Image from "next/image";
+import Container from "./Container";
+
+export default function LogoCloud5() {
+  return (
+    <Container>
+      <div className="grid grid-cols-12 gap-8">
+        <div className="col-span-12 xl:col-span-4">
+          <p className="text-center xl:text-left text-2xl text-horizon-grey-600">
+            Trusted by the world&apos;s most mission driven companies
+          </p>
+        </div>
+        <div className="col-span-12 xl:col-span-8 flex flex-wrap space-x-6 lg:justify-center xl:justify-end items-center">
+          <Image
+            src="/images/sponsors/PUSHER.png"
+            alt="Pusher Logo in Color"
+            width={7000}
+            height={100}
+            className="w-1/6 h-auto"
+          />
+          <Image
+            src="/images/sponsors/MORNINGSTAR.png"
+            alt="Morningstar Logo in Color"
+            width={7000}
+            height={100}
+            className="w-1/6 h-auto"
+          />
+          <Image
+            src="/images/sponsors/BALSAMIQ.png"
+            alt="Balsamiq Logo in Color"
+            width={7000}
+            height={100}
+            className="w-1/6 h-auto"
+          />
+          <Image
+            src="/images/sponsors/MICROSOFT.png"
+            alt="Microsoft Logo in Color"
+            width={7000}
+            height={100}
+            className="w-1/6 h-auto"
+          />
+          <Image
+            src="/images/sponsors/IVY_COLLEGE.png"
+            alt="Ivy College of Business Start Something Logo in Color"
+            width={7000}
+            height={100}
+            className="w-1/6 h-auto"
+          />
+        </div>
+      </div>
+    </Container>
+  );
+}
