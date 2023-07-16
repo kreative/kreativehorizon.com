125b2837d0cb91be6297761b5041392717df99f8
Armaan Gupta <armaan@guppy.im>
2023-07-15 23:34:38 -0500

    fix: added apiVersion

--- sanity.config.ts
+++ sanity.config.ts
@@ -7,6 +7,7 @@ export default defineConfig({
   name: 'default',
   title: 'kreativehorizon.com',
   basePath: "/admin",
+  apiVersion: '2021-03-25',
   projectId: 'laarr84g',
   dataset: 'production',
   plugins: [deskTool(), visionTool()],
