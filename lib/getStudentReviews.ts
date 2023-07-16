de3d67041608ef27700b41af1c070ed1f491866a
Armaan Gupta <armaan@guppy.im>
2023-07-15 23:36:04 -0500

    feat: added StudentReview type

---
+++ lib/getStudentReviews.ts
@@ -0,0 +1,24 @@
+import { createClient, groq } from "next-sanity";
+import StudentReview from "../types/StudentReview";
+
+export async function getStudentReviews(): Promise<StudentReview[]> {
+  const client = createClient({
+    dataset: 'production',
+    apiVersion: '2021-03-25',
+    projectId: 'laarr84g', 
+  });
+
+  return client.fetch(
+    groq`*[ _type == "testimonial" && type == "student" ]{
+      _id,
+      _createdAt,
+      fullName,
+      organization,
+      rating,
+      "photo": photo.asset->url,
+      "avatar": avatar.asset->url,
+      testimonial,
+    }
+    `
+  );
+}
---
+++ types/StudentReview.ts
@@ -0,0 +1,10 @@
+export default interface StudentReview {
+  _id: string;
+  _createdAt: string;
+  fullName: string;
+  organization: string;
+  rating: number;
+  photo: string;
+  avatar: string;
+  testimonial: string;
+}
