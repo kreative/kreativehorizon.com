13b6cf8013ac9bc8cce8c1a37a2dbce3172ed575
Armaan Gupta <armaan@guppy.im>
2023-07-17 00:14:15 -0500

    fix: cloudimage

--- components/CloudImage.tsx
+++ components/CloudImage.tsx
@@ -1,7 +1,9 @@
-
-
-export default function CloudImage({ thumbnailId, className }: { thumbnailId: string, className: string }) {
-  return (
-    
-  )
+export default function CloudImage({
+  thumbnailId,
+  className,
+}: {
+  thumbnailId: string;
+  className: string;
+}) {
+  return <div></div>;
 }
