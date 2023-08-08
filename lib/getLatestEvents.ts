ed613247b3319b244562bf92a1ab550b40d7eb33
Armaan Gupta <armaan@guppy.im>
2023-08-08 03:48:28 -0500

    feat: excluding event feature

--- lib/getLatestEvents.ts
+++ lib/getLatestEvents.ts
@@ -2,11 +2,13 @@ import { groq } from "next-sanity";
 import sanityClient from "./sanityClient";
 import EventCardType from "../types/EventCard";
 
-const getLatestEvents = async (): Promise<EventCardType[]> => {
+const getLatestEvents = async (excludeSlug?: string): Promise<EventCardType[]> => {
   try {
-    // Fetch the StudentReviews using the defined query
-    const events = await sanityClient.fetch(
-      groq`*[ _type == "event" ] | order(start_datetime asc)[0..2] {
+    let events: EventCardType[];
+
+    if (excludeSlug) {
+      events = await sanityClient.fetch(
+        groq`*[ _type == "event" && slug != "${excludeSlug}" && start_datetime > now()] | order(start_datetime asc)[0..2] {
         _id,
         _createdAt,
         title,
@@ -22,7 +24,28 @@ const getLatestEvents = async (): Promise<EventCardType[]> => {
         alt,
       }
       `
-    );
+      );
+      console.log('events', events)
+    } else {
+      events = await sanityClient.fetch(
+        groq`*[ _type == "event" && start_datetime > now() ] | order(start_datetime asc)[0..2] {
+        _id,
+        _createdAt,
+        title,
+        tagline,
+        season,
+        year,
+        start_datetime,
+        end_datetime,
+        date_label,
+        location,
+        slug,
+        cld_id,
+        alt,
+      }
+      `
+      );
+    }
     return events;
   } catch (error) {
     console.error('Error fetching events:', error);
