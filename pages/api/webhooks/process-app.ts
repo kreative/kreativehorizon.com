0187d928cba983c3d4c84e04bd54f19a42e3f622
Armaan Gupta <armaan@guppy.im>
2023-08-17 15:12:20 -0500

    feat: webhook attempt 1 for application completion

---
+++ pages/api/webhooks/process-app.ts
@@ -0,0 +1,49 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import MailerLite from '@mailerlite/mailerlite-nodejs';
+import logger from "@/lib/logger";
+
+const mailerlite = new MailerLite({
+  api_key: process.env.MAILERLITE_TOKEN!,
+});
+
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
+  logger.info("Lead request initiated", { req });
+  const event = req.body.form_response.hidden.event;
+  const email = req.body.form_response.hidden.email;
+
+  mailerlite.groups.get({
+    limit: 25,
+    page: 1,
+    filter: {
+      // @ts-ignore
+      name: event + "-applicants"
+    },
+    sort: "-name",
+  })
+    .then(response => {
+      console.log(response)
+      const groupId = response.data.data[0].id;
+      logger.info("retrieved groups from MailerLite", { req, response });
+      mailerlite.subscribers.createOrUpdate({
+        email,
+        groups: [groupId.toString()],
+      })
+        .then((addResponse) => {
+          console.log(addResponse);
+          logger.info("added subscriber to MailerLite", { req, addResponse });
+          return res.status(200).send("Completed.");
+        })
+        .catch((addError) => {
+          // we need to notify the business that the lead was not collected and to fix this
+          console.log(addError)
+          logger.error("error adding subscriber to MailerLite", { req, addError });
+          return res.redirect(301, `/events/${event}?lead_success=false`);
+        });
+    })
+    .catch(error => {
+      // we need to notify the business that the lead was not collected and to fix this
+      console.log(error);
+      logger.error("error retrieving groups from MailerLite", { req, error });
+      return res.redirect(301, `/events/${event}?lead_success=false`);
+    });
+}
