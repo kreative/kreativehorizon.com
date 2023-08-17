f2d9d4a8cfa4b750ac84ac3d9be92e50491e70b3
Armaan Gupta <armaan@guppy.im>
2023-08-17 12:30:01 -0500

    feat: setup complete app page

--- pages/events/[slug]/complete-app.tsx
+++ pages/events/[slug]/complete-app.tsx
@@ -1,22 +1,43 @@
-import Container from '@/components/Container';
-import Navbar from '@/components/Navbar';
-import Footer from '@/components/Footer';
-import { useRouter } from 'next/router';
-import { NextSeo } from 'next-seo';
+import { useRouter } from "next/router";
+import { Widget } from "@typeform/embed-react";
+import Container from "@/components/Container";
+import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
+import { NextSeo } from "next-seo";
 
 export default function CompleteApp() {
+  const router = useRouter();
+
   return (
-      <div>
-        <NextSeo
-          title="Complete Your Application"
-          />
-        <Navbar />
-        <main>
+    <div>
+      {router.query.form_id && (
+        <div>
+          <NextSeo title="Complete Your Application" />
+          <Navbar />
+          <main>
             <Container>
-                <div></div>
+              <div className="text-center pt-4 md:pt-8 pb-12">
+                <h1 className="text-4xl md:text-5xl font-chedros">
+                  Finish your application to secure your spot!
+                </h1>
+                <p className="text-lg md:text-xl text-horizon-grey-700 pb-6">
+                  We promise it&apos;ll only take{" "}
+                  <span className="font-bold">2 minutes</span> (or we&apos;ll
+                  buy you dinner!)
+                </p>
+                <div className="border border-horizon-purple rounded-xl">
+                  <Widget
+                    id={router.query.form_id!.toString()}
+                    style={{ width: "100%", height: "60vh" }}
+                    inlineOnMobile={true}
+                  />
+                </div>
+              </div>
             </Container>
-        </main>
-        <Footer />
-      </div>
+          </main>
+          <Footer />
+        </div>
+      )}
+    </div>
   );
 }
