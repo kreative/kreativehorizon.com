86d79f4af8491b13082a83da8fb8878964f57e3b
Armaan Gupta <armaan@guppy.im>
2023-08-17 15:11:58 -0500

    ref: added hidden field support

--- pages/events/[slug]/complete-app.tsx
+++ pages/events/[slug]/complete-app.tsx
@@ -31,6 +31,7 @@ export default function CompleteApp() {
                       id={router.query.form_id!.toString()}
                       style={{ width: "100%", height: "60vh" }}
                       inlineOnMobile={true}
+                      transitiveSearchParams={["email"]}
                     />
                   </div>
                 </div>
