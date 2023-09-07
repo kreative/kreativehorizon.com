aa132800335a685652820bb55da8408f19d23550
Armaan Gupta <armaan@guppy.im>
2023-09-07 12:18:20 -0500

    ref: added trustpulse again

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -18,6 +18,11 @@ export default function Document() {
       <body>
         <Main />
         <NextScript />
+        <Script id="trust-pulse">
+          {`
+          !function(w,d,s,n,r,a){(w._tpq=w._tpq||[]).push(['init',n]),(r=d.createElement(s)).type='text/javascript',r.src='https://a.trstplse.com/app/js/api.min.js',r.async=!0,(a=d.getElementsByTagName(s)[0]).parentNode.insertBefore(r,a)}(window,document,'script',10418);
+          `}
+        </Script>
         <noscript
             dangerouslySetInnerHTML={{
               __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5LZKZVDL" height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
